0bba5ccded7fa9f05d423acf67a565d0d31ce9ab
Santiago <Santiagogbf2000@Gmail.com>
2023-09-03 15:17:02 -0400

    Solucionado problema de index

--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import Navbar from '@/components/front/Navbar.vue';
-import Home from '@/components/front/Home.vue';
-import Category from '@/components/front/Category.vue';
-import TrickOrTreat from '@/components/front/TrickOrTreat.vue';
-import Subscribe from '@/components/front/Subscribe.vue';
-import Footer from '@/components/front/Footer.vue';
-import { Head} from '@inertiajs/vue3';
+import Navbar from "@/components/front/Navbar.vue";
+import Home from "@/components/front/Home.vue";
+import Category from "@/components/front/Category.vue";
+import TrickOrTreat from "@/components/front/TrickOrTreat.vue";
+import Subscribe from "@/components/front/Subscribe.vue";
+import Footer from "@/components/front/Footer.vue";
+import { Head } from "@inertiajs/vue3";
+import MySwiper from "@/components/front/MySwiper.vue";
 
 defineProps({
     canLogin: {
@@ -14,60 +15,60 @@ defineProps({
     canRegister: {
         type: Boolean,
     },
-    colors:{
-      type: Object,
-      required: false
-    }
+    colors: {
+        type: Object,
+        required: false,
+    },
 });
-
-
 </script>
 
-
-
 <template>
-  
-  <Head title="Home"/>
-  <div class="container overflow-x-hidden" :style="{background: colors ? colors.color1 : '', }">
-    
-
-    
-    <Navbar :canLogin="canLogin" :canRegister="canRegister" :isAuth="$page.props.auth.user" :colors="colors"/>
-    <div 
-    class="flex flex-col mt-20" v-if="colors">
-      <p
-      :style="{color: colors.color4}"></p>
-      <div class="w-60 h-12 flex">
-        <div class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]" 
-        :style="{backgroundColor: colors.color1}"
-        :title="colors.color1"
-        ></div>
-        <div class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]" 
-        :style="{backgroundColor: colors.color2}"
-        :title="colors.color2"
-        ></div>
-        <div class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]" 
-        :style="{backgroundColor: colors.color3}"
-        :title="colors.color3"
-        ></div>
-        <div class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]" 
-        :style="{backgroundColor: colors.color4}"
-        :title="colors.color4"
-        ></div>
-        <div class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]" 
-        :style="{backgroundColor: colors.color5}"
-        :title="colors.color5"
-        ></div>
-      </div>
+    <Head title="Home" />
+    <div
+        class="container overflow-x-hidden"
+        :style="{ background: colors ? colors.color1 : '' }"
+    >
+        <Navbar
+            :canLogin="canLogin"
+            :canRegister="canRegister"
+            :isAuth="$page.props.auth.user"
+            :colors="colors"
+        />
+        <div class="flex flex-col mt-20" v-if="colors">
+            <p :style="{ color: colors.color4 }"></p>
+            <div class="w-60 h-12 flex">
+                <div
+                    class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]"
+                    :style="{ backgroundColor: colors.color1 }"
+                    :title="colors.color1"
+                ></div>
+                <div
+                    class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]"
+                    :style="{ backgroundColor: colors.color2 }"
+                    :title="colors.color2"
+                ></div>
+                <div
+                    class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]"
+                    :style="{ backgroundColor: colors.color3 }"
+                    :title="colors.color3"
+                ></div>
+                <div
+                    class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]"
+                    :style="{ backgroundColor: colors.color4 }"
+                    :title="colors.color4"
+                ></div>
+                <div
+                    class="rounded-full border-solid border-2 border-black h-12 w-12 bg-[#722727]"
+                    :style="{ backgroundColor: colors.color5 }"
+                    :title="colors.color5"
+                ></div>
+            </div>
+        </div>
+        <Home :colors="colors" />
+        <Category :colors="colors" />
+        <TrickOrTreat />
+        <Subscribe />
+        <Footer />
+        <p class="text-black">{{ colors }}</p>
     </div>
-
-    
-
-    <Home :colors="colors"/>
-    <Category :colors="colors"/>
-    <TrickOrTreat />
-    <Subscribe />
-    <Footer />
-    <p class="text-black">{{ colors }}</p>
-  </div>
 </template>
--- resources/js/components/front/Home.vue
+++ resources/js/components/front/Home.vue
@@ -1,32 +1,66 @@
 <script setup>
-  import { gradientBgBase } from "@/colors";
+import { gradientBgBase } from "@/colors";
 import MySwiper from "@/components/front/MySwiper.vue";
 
-  defineProps({
-  colors:{
-    type: Object,
-    required: false,
-  }
+defineProps({
+    colors: {
+        type: Object,
+        required: false,
+    },
 });
- 
 </script>
 
 <template>
-  <section id="home" class="grid place-items-center overflow-hidden">
-    <div class="grid grid-flow-row-dense grid-cols-1 lg:grid-cols-2 items-end w-full px-0 lg:px-10 place-items-center text-center p-4 rounded-md mt-20 text-black bg-gradient-to-b from-primary-200/20 to-primary-200/10" :style="{background: colors ? colors.color3 : '',}" >
-      <div class="text-5xl md:text-6xl font-bold text-white w-full pb-20 order-2 lg:order-1" :style="{color: colors ? colors.color4 : '',}">
-        <div class="flex items-center justify-center gap-3">
-          <img class="w-1/6" src="../assets/images/head1-img.png" alt="head1-img">
+    <section id="home" class="grid place-items-center overflow-hidden">
+        <div
+            class="grid grid-flow-row-dense grid-cols-1 lg:grid-cols-2 items-end w-full px-0 lg:px-10 place-items-center text-center p-4 rounded-md mt-20 text-black bg-gradient-to-b from-primary-200/20 to-primary-200/10"
+            :style="{ background: colors ? colors.color3 : '' }"
+        >
+            <div
+                class="text-5xl md:text-6xl font-bold text-white w-full pb-20 order-2 lg:order-1"
+                :style="{ color: colors ? colors.color4 : '' }"
+            >
+                <div class="flex items-center justify-center gap-3">
+                    <img
+                        class="w-1/6"
+                        src="../assets/images/head1-img.png"
+                        alt="head1-img"
+                    />
+                </div>
+                <!-- <h2 class="text-4xl md:text-5xl justify-center items-center">HALLOW<span class="text-secondary">ee</span>D</h2> -->
+                <h4 class="pt-5">¡Las mejores camisas las tenemos nosotros!</h4>
+            </div>
+            <div class="w-full h-full order-1 lg:order-2">
+                <MySwiper :colors="colors" />
+            </div>
         </div>
-        <!-- <h2 class="text-4xl md:text-5xl justify-center items-center">HALLOW<span class="text-secondary">ee</span>D</h2> -->
-        <h4 class="pt-5">¡Las mejores camisas las tenemos nosotros!</h4>
-      </div>
-      <div class="w-full h-full order-1 lg:order-2">
-        <MySwiper :colors="colors"/>
-      </div>
-    </div>
-  </section>
-  
-  <!-- <Table /> -->
-  
+    </section>
+
+    <!-- <Table /> -->
 </template>
+
+<style>
+@media (min-width: 768px) {
+    .container {
+        max-width: 100%;
+    }
+
+    @media (min-width: 1024px) {
+        .container {
+            max-width: 100%;
+        }
+    }
+
+    @media (min-width: 1280px) {
+        .container {
+            max-width: 100%;
+        }
+    }
+
+    @media (min-width: 1536px) {
+        .container {
+            max-width: 100%;
+        }
+    }
+}
+</style>
